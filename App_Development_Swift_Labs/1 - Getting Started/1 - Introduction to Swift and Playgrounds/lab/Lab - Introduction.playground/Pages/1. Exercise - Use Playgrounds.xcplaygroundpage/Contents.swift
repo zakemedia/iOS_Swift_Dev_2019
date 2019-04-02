88ac9d852d284a6d1ee88ddf036ae13c22968204
Welbz <hello@zakemedia.com.au>
2019-04-03 00:19:33 +1100

    Lab 1.1
    Lab 1.1

--- App_Development_Swift_Labs/1 - Getting Started/1 - Introduction to Swift and Playgrounds/lab/Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
+++ App_Development_Swift_Labs/1 - Getting Started/1 - Introduction to Swift and Playgrounds/lab/Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
@@ -10,12 +10,14 @@ print("How to use playgrounds to make writing Swift fun and simple")
 /*:
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
-
+print ("Welby is back in 2019")
 
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.
  */
-
+print ("Welby is back in 2019")
+print ("Im Back Baby")
+print ("Im Back")
 
 
 /*:
