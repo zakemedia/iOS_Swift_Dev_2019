236a73a305401feea71723517a26c63c47390f9b
Welbz <hello@zakemedia.com.au>
2019-05-03 00:41:59 +1000

    Labs 1.4 (Part 4)
    Labs 1.4 (Part 4)

--- App_Development_Swift_Labs/1 - Getting Started/4 - Control Flow/lab/Lab - Control Flow.playground/Pages/4. Exercise - Boolean Practice.xcplaygroundpage/Contents.swift
+++ App_Development_Swift_Labs/1 - Getting Started/4 - Control Flow/lab/Lab - Control Flow.playground/Pages/4. Exercise - Boolean Practice.xcplaygroundpage/Contents.swift
@@ -11,9 +11,13 @@
 
 let hasFish = true
 let hasPizza = false
-let hasVegan = true
+let hasVegan = false
 
-
+if (hasFish || hasPizza) && hasVegan {
+    print ("Let's go!")
+    } else {
+    print ("Sorry, we'll have to think of somewhere else.")
+}
 /*:
  Imagine you're trying to decide whether or not to go on a walk. You decide that you'll go on a walk if it's not raining or if it's 82 degress or warmer and sunny out. Create a constant `isNiceWeather` that is equal to an expression that evaluates to a boolean indicating whether or not the weather is nice enough for you to go for a walk. Write an if statement that will print "I'm going for a walk!" if the weather is nice.
  */
