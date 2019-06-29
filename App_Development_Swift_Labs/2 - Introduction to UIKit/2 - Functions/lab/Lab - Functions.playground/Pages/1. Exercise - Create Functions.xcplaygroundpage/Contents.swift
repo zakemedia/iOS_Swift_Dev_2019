645576b3beaa95b7b92f3e4459144bb4948b33a5
Welbz <hello@zakemedia.com.au>
2019-06-30 00:09:55 +1000

    iOS Labs 2.2 (Parts 1,2,3)
    iOS Labs 2.2 (Parts 1,2,3)

--- App_Development_Swift_Labs/2 - Introduction to UIKit/2 - Functions/lab/Lab - Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
+++ App_Development_Swift_Labs/2 - Introduction to UIKit/2 - Functions/lab/Lab - Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
@@ -3,11 +3,40 @@
  
  Write a function called `introduceMyself` that prints a brief introduction of yourself. Call the function and observe the printout.
  */
+func introduceMyself() {
+    print("I am printing a function. woooo!")
+}
 
-
+introduceMyself()
 /*:
  Write a function called `magicEightBall` that generates a random number and then uses either a switch statement or if-else-if statements to print different responses based on the random number generated. `let randomNum = Int.random(in: 0...4)` will generate a random number from 0 to 4, after which you can print different phrases corresponding to the number generated. Call the function multiple times and observe the different printouts.
  */
+func magicEightBall () {
+    let randomNum = Int.random(in: 0...4)
+    
+    switch randomNum {
+    case 0:
+        print ("the random number should be 0. Random number = \(randomNum)")
+    case 1:
+        print ("the random number should be 1. Random number = \(randomNum)")
+    case 2:
+        print ("the random number should be 2. Random number = \(randomNum)")
+    case 3:
+        print ("the random number should be 3. Random number = \(randomNum)")
+    case 4:
+        print ("the random number should be 4. Random number = \(randomNum)")
+    default:
+        print ("Uh Oh! Something went wrong")
+    }
+}
 
+magicEightBall ()
+magicEightBall ()
+magicEightBall ()
+magicEightBall ()
+magicEightBall ()
+magicEightBall ()
+magicEightBall ()
+magicEightBall ()
 
 //: page 1 of 6  |  [Next: App Exercise - A Functioning App](@next)
