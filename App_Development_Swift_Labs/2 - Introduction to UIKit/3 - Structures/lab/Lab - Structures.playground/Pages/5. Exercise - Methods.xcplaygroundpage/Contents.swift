c02afeb1776aae3e03ef858bc7b4153af7b059d5
Welbz <hello@zakemedia.com.au>
2019-08-03 22:51:01 +1000

    iOS Labs 2.3 (Structs Part 5)
    Mutating functions

--- App_Development_Swift_Labs/2 - Introduction to UIKit/3 - Structures/lab/Lab - Structures.playground/Pages/5. Exercise - Methods.xcplaygroundpage/Contents.swift
+++ App_Development_Swift_Labs/2 - Introduction to UIKit/3 - Structures/lab/Lab - Structures.playground/Pages/5. Exercise - Methods.xcplaygroundpage/Contents.swift
@@ -8,7 +8,22 @@ struct Book {
     var author: String
     var pages: Int
     var price: Double
+    
+    // instance method
+    func description() {
+        print("Books Facts:")
+        print(title)
+        print(author)
+        print(pages)
+        print(price)
+    }
 }
+
+let someBook = Book(title: "Lord of the Rings", author: "JRR Tolken", pages: 974, price: 49.99)
+//someBook is instances of Book type
+
+someBook.description()
+//instance method can be called on all instances of Book
 /*:
  A `Post` struct has been created for you below, representing a generic social media post. Add a mutating method on `Post` called `like` that will increment `likes` by one. Then create an instance of `Post` and call `like()` on it. Print out the `likes` property before and after calling the method to see whether or not the value was incremented.
  */
@@ -16,5 +31,15 @@ struct Post {
     var message: String
     var likes: Int
     var numberOfComments: Int
+    
+    mutating func like() {
+        likes += 1
+    }
 }
-//: [Previous](@previous)  |  page 5 of 10  |  [Next: App Exercise - Workout Functions](@next)
+
+var myPost = Post(message: "Hi", likes: 0, numberOfComments: 0)
+print(myPost.likes)
+myPost.like()
+print(myPost.likes)
+myPost.like()
+print(myPost.likes)
