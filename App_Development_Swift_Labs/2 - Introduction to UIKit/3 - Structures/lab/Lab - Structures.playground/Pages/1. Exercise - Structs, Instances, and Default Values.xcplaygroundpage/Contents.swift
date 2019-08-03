fa10510c22bc44fae07755d65912da4bb1f3997d
Welbz <hello@zakemedia.com.au>
2019-08-03 19:09:56 +1000

    iOS Labs 2.2 (Structs Part 1)
    iOS Labs 2.2 (Structs Part 1)

--- App_Development_Swift_Labs/2 - Introduction to UIKit/3 - Structures/lab/Lab - Structures.playground/Pages/1. Exercise - Structs, Instances, and Default Values.xcplaygroundpage/Contents.swift
+++ App_Development_Swift_Labs/2 - Introduction to UIKit/3 - Structures/lab/Lab - Structures.playground/Pages/1. Exercise - Structs, Instances, and Default Values.xcplaygroundpage/Contents.swift
@@ -3,26 +3,36 @@
  
  Imagine you are creating an app that will monitor location. Create a `GPS` struct with two variable properties, `latitude` and `longitude`, both with default values of 0.0.
  */
-
+struct GPS {
+    var latitude: Double = 0.0
+    var longitude: Double = 0.0
+}
 
 /*:
  Create a variable instance of `GPS` called `somePlace`. It should be initialized without supplying any arguments. Print out the latitude and longitude of `somePlace`, which should be 0.0 for both.
  */
-
-
+var somePlace = GPS()
+print(somePlace.latitude)
+print(somePlace.longitude)
 /*:
  Change `somePlace`'s latitude to 51.514004, and the longitude to 0.125226, then print the updated values.
  */
-
-
+somePlace.latitude = 51.514004
+somePlace.longitude = 0.125226
+print("\(somePlace.latitude) and \(somePlace.longitude)")
 /*:
  Now imagine you are making a social app for sharing your favorite books. Create a `Book` struct with four variable properties: `title`, `author`, `pages`, and `price`. The default values for both `title` and `author` should be an empty string. `pages` should default to 0, and `price` should default to 0.0.
  */
-
+struct Book {
+    var title: String
+    var author: String
+    var pages: Int
+    var price: Double
+}
 
 /*:
  Create a variable instance of `Book` called `favoriteBook` without supplying any arguments. Print out the title of `favoriteBook`. Does it currently reflect the title of your favorite book? Probably not. Change all four properties of `favoriteBook` to reflect your favorite book. Then, using the properties of `favoriteBook`, print out facts about the book.
  */
-
-
+var favoriteBook = Book(title: "The Hobbit", author: "JRR Tolken", pages: 310, price: 29.99)
+print("My fav books is called \(favoriteBook.title) by \(favoriteBook.author), which has \(favoriteBook.pages) pages and costs \(favoriteBook.price).")
 //: page 1 of 10  |  [Next: App Exercise - Workout Tracking](@next)
